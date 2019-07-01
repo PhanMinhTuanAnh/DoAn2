7f5fe6eb77b979bad60c8a3008066b87c6c10257
PhanMinhTuanAnh <44412737+PhanMinhTuanAnh@users.noreply.github.com>
2019-07-01 17:32:42 +0700

    Update 1/7/2019
    Nâng được hiệu suất OVO lên 83%

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Đồ án 2 
+﻿# Đồ án 2 
 
 	. Tên đề tài : Tìm hiểu Máy vector hỗ trợ và xây dựng chương trình nhận dạng chữ số viết tay
 	. Yêu cầu : Ngôn ngữ Python, Chạy bằng console	
 	. Input : Dữ liệu chữ viết tay đã có nhãn https://www.kaggle.com/c/digit-recognizer
 	. Ouput : Nhận dạng chữ số viết tay chưa có nhãn
 	. Thời gian train : 10p
-	#best-performance : OVR:80% , OVO:79%??, khá thấp , vẫn đang cải tiến ...
+	#best-performance : OVR:80% , OVO:83%, khá thấp , vẫn đang cải tiến ...
--- final.py
+++ final.py
@@ -30,34 +30,54 @@ def predict_class(X, classifiers):
         	pos = pre+1
         _predict = predict(clf,X)
         if(_predict>0):
-        	predictions[pos]=predictions[pos]+abs(_predict)
+        	predictions[pos]=predictions[pos]+1
+        	#predictions[pos]=max(predictions[pos],abs(_predict))
+        	#predictions[pos]=predictions[pos]+abs(_predict)
         else:
-        	predictions[pre]=predictions[pre]+abs(_predict)
+        	predictions[pre]=predictions[pre]+1
+        	#predictions[pre]=max(predictions[pre],abs(_predict))
+        	#predictions[pre]=predictions[pre]+abs(_predict)
         pos = pos + 1
 
     out = np.argmax(predictions[:])
     return out
 #----------------------------------------------
 
-training_dataframe = pd.read_csv('Input/train.csv')
-rcount = int(0.5*training_dataframe.shape[0])
-subset_training_dataframe = training_dataframe.sample(n=rcount)
-X = subset_training_dataframe.drop("label", axis = 1)
-y = subset_training_dataframe.label.values.astype(int)
-X = scale(X)
-sum = 0
-for i in range(0,X.shape[0]):
-	y_predict = predict_class(X[i,:],classifiers)
-	print("predict:")
-	print(y_predict)
-	print("real:")
-	print(y[i])
-	if(y_predict==y[i]):
-		sum = sum + 1
-print(sum/y.shape[0])
+# training_dataframe = pd.read_csv('Input/train.csv')
+# rcount = int(0.5*training_dataframe.shape[0])
+# subset_training_dataframe = training_dataframe.sample(n=rcount)
+# X = subset_training_dataframe.drop("label", axis = 1)
+# y = subset_training_dataframe.label.values.astype(int)
+# X = scale(X)
+# sum = 0
+# for i in range(0,X.shape[0]):
+# 	y_predict = predict_class(X[i,:],classifiers)
+# 	print("predict:")
+# 	print(y_predict)
+# 	print("real:")
+# 	print(y[i])
+# 	if(y_predict==y[i]):
+# 		sum = sum + 1
+# print(sum/y.shape[0])
 
 
-# img = cv2.imread('28x28.png',0)
+
+
+
+#--------------Predict_TEST.csv----------
+testing_dataframe = pd.read_csv('Input/test.csv')
+rcount = int(0.005*testing_dataframe.shape[0])
+testing_dataframe = testing_dataframe.sample(n=rcount)
+testing_dataframe = scale(testing_dataframe)
+print(testing_dataframe[1])
+for i in range(0,10):
+	_2d = testing_dataframe[i].reshape(28,28)
+	plt.title(f'Predicted Label: {predict_class(testing_dataframe[i],classifiers)}')
+	plt.imshow(_2d)
+	plt.show()
+
+#-------------DRAWimg-----------------
+# img = cv2.imread('28x28(1).png',0)
 # imgplt = plt.imshow(img,cmap='gray')
 # plt.show()
 
@@ -71,3 +91,6 @@ print(sum/y.shape[0])
 
 # print(np.shape(imgg))
 # print(predict_class(imgg,classifiers))
+#------------------------------------------
+
+
